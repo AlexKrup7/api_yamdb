9227a83bd031b4e646149e75374647fa0a7c09f9
Mikhail Stepanov <slake6@yandex.ru>
2021-09-10 01:27:29 +0300

    another fixies

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -8,8 +8,10 @@ router_v1.register('users', UserViewSet)
 router_v1.register(r'genres', GenreViewSet, basename='genre')
 router_v1.register(r'categories', CategoryViewSet, basename='category')
 router_v1.register(r'titles', TitleViewSet, basename='title')
-router_v1.register(r'reviews', ReviewViewSet, basename='review')
-router_v1.register(r'comments', CommentViewSet, basename='comment')
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
+                   ReviewViewSet, basename='review')
+router_v1.register(r'titles/(?P<title_id>\d+)/(?P<review_id>\d+)/comments',
+                   CommentViewSet, basename='comment')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
